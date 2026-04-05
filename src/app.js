d9272ed4bbbe460cece5e2f110bfb65d6501ad4f
Mir Moniruzzaman <mirmuniruzzaman303@gmail.com>
2026-04-05 21:22:29 +0600

    class is done

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+import cookieParser from 'cookie-parser'
 import express from 'express'
+import authRoute from './modules/auth/auth.route'
 const app=express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use("/api/auth",authRoute)
 export default app
